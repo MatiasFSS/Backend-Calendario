6809f96760f90fd644c03a9a14a3ce3698528875
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-27 16:51:19 -0300

    Recuperar información del posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,23 +1,42 @@
 const {response} = require('express')
 
 const crearUsuario = (req, res = response) => {
+    
+    const {name, email, password} = req.body
+
+    if(name.length < 5){
+        return res.status(400).json({
+            ok:false,
+            msg:'El nombre debe ser de 5 letras'
+        })
+    }
+
     res.json({
         ok:true,
-        msg:'Register'
+        msg:'Register',
+        name,
+        email,
+        password
     })
 }
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req, res = response) => {
+    const {email, password} = req.body
+
     res.json({
         ok:true,
-        msg:'Login'
+        msg:'Login',
+        email,
+        password
+        
     })
 }
 
-const revalidarToken = (req, res) => {
+const revalidarToken = (req, res = response) => {
     res.json({
         ok:true,
-        msg:'renew'
+        msg:'renew',
+
     })
 }
 
