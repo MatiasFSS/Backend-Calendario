03560a64fe5e14f1395bf2e264b8ac4315d0689c
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-27 17:19:04 -0300

    Creación de custom middleware para manejo de errores

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,16 +5,6 @@ const crearUsuario = (req, res = response) => {
     
     const {name, email, password} = req.body
 
-    //Manejo de errores
-    const errors = validationResult(req)
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        })
-    }
-
     res.status(201).json({
         ok:true,
         msg:'Register',
@@ -27,16 +17,6 @@ const crearUsuario = (req, res = response) => {
 const loginUsuario = (req, res = response) => {
 
     const {email, password} = req.body
-
-    //Manejo de errores
-    const errors = validationResult(req)
-
-    if(!errors.isEmpty()){
-        return res.status(400).json({
-            ok:false,
-            errors: errors.mapped()
-        })
-    }
 
     res.status(201).json({
         ok:true,
