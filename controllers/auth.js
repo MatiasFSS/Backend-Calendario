7b91cb4fd3a1972225eef713230fbcc4e7083dcd
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-28 16:35:15 -0300

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -40,17 +40,49 @@ const crearUsuario = async(req, res = response) => {
     
 }
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async(req, res = response) => {
 
     const {email, password} = req.body
 
-    res.status(201).json({
-        ok:true,
-        msg:'Login',
-        email,
-        password
+    try {
+
+        const usuario = await Usuario.findOne({email})
+        if(!usuario){
+            return res.status(400).json({
+                ok:false,
+                msg: 'Usuario no existe con ese email'
+            })
+        }
+
+        //confirmar password
+
+        const validPassword = bcrypt.compareSync(password, usuario.password)
+
+        if(!validPassword){
+            return res.status(400).json({
+                ok:false,
+                msg: 'Password incorrecto'
+            })
+        }
+
+        //Generar JWT
+
+        res.json({
+            ok:true,
+            uid: usuario.id,
+            name: usuario.name,
+
+
+        })
+
         
-    })
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        })
+    }
 }
 
 const revalidarToken = (req, res = response) => {
