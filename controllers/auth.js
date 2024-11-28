eaa0f0b608ec77a29080cacb81db9d7a4534da52
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-28 17:15:00 -0300

    Generar JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const {response} = require('express')
 const Usuario = require('../models/Usuario')
 const bcrypt = require('bcryptjs')
+const {generarJWT} = require('../helpers/jwt')
 
 const crearUsuario = async(req, res = response) => {
     
@@ -22,11 +23,14 @@ const crearUsuario = async(req, res = response) => {
         usuario.password = bcrypt.hashSync(password, salt)
 
         await usuario.save()
+        //Generar JWT
+        const token = await generarJWT(usuario.id, usuario.name)
 
         res.status(201).json({
             ok:true,
             uid: usuario.id,
-            name: usuario.name
+            name: usuario.name,
+            token
 
         })
 
@@ -66,11 +70,13 @@ const loginUsuario = async(req, res = response) => {
         }
 
         //Generar JWT
+        const token = await generarJWT(usuario.id, usuario.name)
 
         res.json({
             ok:true,
             uid: usuario.id,
             name: usuario.name,
+            token
 
 
         })
