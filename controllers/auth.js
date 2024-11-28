abae29c9f5d9361e9a5dd8d57d145bede0657cd1
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-11-28 17:37:45 -0300

    Revalidar JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -91,12 +91,21 @@ const loginUsuario = async(req, res = response) => {
     }
 }
 
-const revalidarToken = (req, res = response) => {
+const revalidarToken = async(req, res = response) => {
+
+    const uid = req.uid
+    const name = req.name
+
+    //Generar JWT
+    const token = await generarJWT(uid, name)
+
     res.json({
         ok:true,
-        msg:'renew',
+        token
 
     })
+
+    
 }
 
 module.exports = {
