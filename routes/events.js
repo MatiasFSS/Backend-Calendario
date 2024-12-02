d64284eedd08198ffb487e7832af1ddc6445a9c3
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-12-02 15:38:51 -0300

    Creación modelo Evento y Validación campos para los eventos

--- routes/events.js
+++ routes/events.js
@@ -6,9 +6,11 @@
 const {Router} = require('express')
 const {validarJWT} = require('../middlewares/validar-jwt')
 const router = Router()
-
+const {check} = require('express-validator')
+const {validarCampos} = require('../middlewares/validar-campos')
 
 const {getEventos, crearEvento, actualizarEvento, eliminarEvento} = require('../controllers/events')
+const { isDate } = require('../helpers/isDate')
 
 //Todas tienen que pasar la validacion del jwt
 router.use(validarJWT)
@@ -17,7 +19,16 @@ router.use(validarJWT)
 router.get('/' , getEventos)
 
 //Crear un nuevo evento
-router.post('/', crearEvento)
+router.post(
+    '/', 
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatorio').custom( isDate ),
+        check('end', 'Fecha de finalización es obligatorio').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento,
+)
 
 //Crear un actualizar evento
 router.put('/:id', actualizarEvento)
