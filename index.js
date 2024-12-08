ce5f06f827b16d76fc6702eeabc3bd67feb4d98f
Matías Fabian Sánchez Soto <matias.fss97@gmail.com>
2024-12-08 16:09:57 -0300

    React App desplegada

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const { dbConection } = require('./database/config')
 const cors = require('cors')
@@ -23,6 +24,10 @@ app.use(express.json())
 //Rutas
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/events', require('./routes/events'))
+
+app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 //TODO:CRUD:eventos
 
 //Escuchar peticiones
